f84a88f0f071a16841eadff0cfdb9ff8473ebab3
lucas <lucasausbury@gmail.com>
2016-12-04 15:05:49 -0500

    working version
    just needs to be styled and instructions added

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,33 @@ var $root = null, $node_tmp = null, $decd_tmp = null;
 var node_dir = {}, decd_dir = {};
 var discriminants = {}, classes = {};
 
-function renderQuestion( data, counter ) {
-	var $tmp = $node_tmp.clone();
-	data.counter = counter;
-
-	if( parseInt(data.fulcrum) >= 0 ) {
-		console.log( discriminants[data.fulcrum] );
-		console.log( node_dir );
-		$tmp.render(data, node_dir);
-	} 
-
-	return $tmp;
-}
-
 function buildTree() {
-	console.log( "A" );
 	$.getJSON("http://lucasausbury.github.io/dev/decisionTree.json", function( out ) {
-		alert("B");
-		var t = $node_tmp.clone();
+		var t1 = null;
+		var t2 = null;
 		var x = null;
 
-		discriminants = data.discriminants;
-		classes = data.classes;
+		discriminants = out.discriminants;
+		classes = out.classes;
 
 		$root.html('');
-		console.log( out );
 		for( var i in out.data ) {
+			t1 = $node_tmp.clone();
+			t2 = $rslt_tmp.clone();
 			x = out.data[i];
+			x.counter = (parseInt(i) + 1);
 
 			if( parseInt(x.fulcrum) >= 0 )
-				$root.append( t.render(x, node_dir) );
+				var $t = t1.render(x, node_dir);
+			else
+				var $t = t2.render(x, rslt_dir);
+
+			$t.addClass('target_' + x.counter );
+
+			if( x.counter == 1 ) $t.show();
+			else $t.hide();
+
+			$root.append( $t );
 		}
 	});
 }
@@ -40,6 +37,7 @@ $(document).ready(function() {
 	$root = $('#root');
 	$node_tmp = $('.node-template').detach();
 	$decd_tmp = $('.decider-template').detach();
+	$rslt_tmp = $('.result-template').detach();
 	node_dir = {
 		'control-label':{
 			'text':function( params ){
@@ -51,7 +49,12 @@ $(document).ready(function() {
 
 				for( var i in this.branches ) {
 					var $t = $decd_tmp.clone();
-					$t.find('.custom-control-description').text(i);
+					var l = i.replace('<=', 'less than or equal to');
+					l = l.replace('>', 'greater than');
+
+					$t.find('.custom-control-description').text(l);
+					$t.find('input').attr("name", "grp_"+this.counter);
+					$t.attr('data-target', (parseInt(this.branches[i]) + 1));
 
 					$cont.append( $t );
 				}
@@ -61,6 +64,28 @@ $(document).ready(function() {
 		}
 	};
 	decd_dir = {};
+	rslt_dir = {
+		'class_label':{
+			'text':function( params ) {
+				return classes[this.class];
+			}
+		}, 'probability':{
+			'text':function( params ) {
+				return (parseFloat(this.probability) * 100).toFixed(2);
+			}
+		}
+	};
 
 	buildTree();
+
+	$(document).on('click', 'input[type="radio"]', function() {
+		var grandparent = $(this).closest('.node-template, .result-template');
+		var parent = $(this).closest('.decider-template');
+		var target = ".target_" + parent.data('target');
+
+		$('.active').removeClass('active');
+		grandparent.addClass('active');
+		$('.active').nextAll('.node-template, .result-template').slideUp();
+		$(target).slideDown();
+	});
 });
