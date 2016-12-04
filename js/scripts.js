f5c9ba4ca425e3227df97f31eeabe8645b1338d9
lucas <lucasausbury@gmail.com>
2016-12-04 14:11:47 -0500

    changed decision tree json format

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,56 @@
+var $root = null, $node_tmp = null, $decd_tmp = null;
+var node_dir = {}, decd_dir = {};
+var discriminants = {}, classes = {};
+
+function renderQuestion( data, counter ) {
+	var $tmp = $node_tmp.clone();
+	data.counter = counter;
+
+	if( parseInt(data.fulcrum) >= 0 ) {
+		console.log( discriminants[data.fulcrum] );
+		console.log( node_dir );
+		$tmp.render(data, node_dir);
+	} 
+
+	return $tmp;
+}
+
 function buildTree() {
-	console.log("Making attempt...");
-	$.ajax({
-		url:"http://lucasausbury.github.io/dev/decisionTree.json", 
-		dataType:"json",
-		success: function( data ) {
-			console.log( data );
-		}
+	$.getJSON("http://lucasausbury.github.io/dev/decisionTree.json", function( data ) {
+		discriminants = data.discriminants;
+		classes = data.classes;
+
+		$root.html('');
+		$root.append( renderQuestion( data.data, 1 ) );
 	});
 }
 
 $(document).ready(function() {
-	var $root = $('#root');
-	var $node_tmp = $('.node-template').detach();
-	var $decd_tmp = $('.decider-template').detach();
+	$root = $('#root');
+	$node_tmp = $('.node-template').detach();
+	$decd_tmp = $('.decider-template').detach();
+	node_dir = {
+		'control-label':{
+			'text':function( params ){
+				return this.counter + '. ' + discriminants[this.fulcrum];
+			}
+		}, 'control-options':{
+			'html':function( params ) {
+				var $cont = $('<div />');
+				var name = 
+
+				for( var i in this.branches ) {
+					var $t = $decd_tmp.clone();
+					$t.find('.custom-control-description').text(i);
+
+					$cont.append( $t );
+				}
+
+				return $cont.html();
+			}
+		}
+	};
+	decd_dir = {};
 
 	buildTree();
 });
